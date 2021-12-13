87773b1c64ebdca5d45fd916f2307905fc4ea6f8
Anthonny42 <adlp@cin.ufpe.br>
2021-12-13 15:19:16 -0300

    Baixando PDF

--- src/url/App.java
+++ src/url/App.java
@@ -1,7 +1,16 @@
 package url;
+
+import java.io.File;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World....ta pegando ou nao porra.!");
-        System.out.println("sss");
+       String link = "https://www.gov.br/ans/pt-br/arquivos/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-tiss/padrao-tiss/padrao-tiss_componente-organizacional_202111.pdf";
+       
+       File out = new File("C:\\Users\\VALQUIRIA\\Desktop\\ESTÁGIO\\PrimeiroProjeto\\TestandoDownload.pdf");
+
+       new Thread(new Download(link, out)).start();
+
+
+
     }
 }
